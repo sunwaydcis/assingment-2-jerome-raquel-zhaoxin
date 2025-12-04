c22c39b531d8604c27a2b615ef25375d71aa8897
JeromeTanOfficial <85390088+jer-tan@users.noreply.github.com>
2025-12-04 09:37:42 +0800

    Remove commented-out code and streamline `calculateEconomicScore` for readability and maintainability.

--- src/main/scala-3/Question2.scala
+++ src/main/scala-3/Question2.scala
@@ -3,7 +3,7 @@ object Question2 {
   case class EconomicalScore(
                               hotelName: String,
                               averagePrice: Double,
-                            averageDiscount: Double,
+                              averageDiscount: Double,
                               averageProfitMargin: Double
                             )
 
@@ -48,135 +48,11 @@ object Question2 {
       val profitNorm = safeNorm(s.averageProfitMargin, minPM, rangePM)
 
       // invert price so cheaper → better score
-      val combined = (1 - priceNorm + discountNorm + 1- profitNorm) / 3
+      val combined = (1 - priceNorm + discountNorm + 1 - profitNorm) / 3
 
       EconomicScorePercentage(s.hotelName, combined)
     }
 
     Some(finals.maxBy(_.score))
-
-//
-//  def calculateEconomicScore(bookings: List[HotelBooking]): Option[EconomicScorePercentage] =
-//    if bookings.isEmpty then return None
-//
-//    val minPriceOfAllBookings =
-//      bookings.map(b => b.bookingPrice / b.rooms / b.noOfDays).min
-//
-//    val maxPriceOfAllBookings =
-//      bookings.map(b => b.bookingPrice / b.rooms / b.noOfDays).max
-//
-//    val grouped = bookings.groupBy(_.hotelName)
-//
-//    val scores: List[EconomicalScore] = grouped.map { case (hotelName, hotelBookings) =>
-//      val effectivePrices =
-//        hotelBookings.map(b => b.bookingPrice / b.rooms / b.noOfDays)
-//
-//      val avgPrice = effectivePrices.sum / effectivePrices.length
-//
-//      val avgDiscount = hotelBookings.map(_.discount).sum / hotelBookings.length
-//      val avgProfitMargin = hotelBookings.map(_.profitMargin).sum / hotelBookings.length
-//
-//      EconomicalScore(hotelName, avgPrice, avgDiscount, avgProfitMargin)
-//    }.toList
-//
-//    val minAveragePrice = scores.map(_.averagePrice).min
-//    val maxAveragePrice = scores.map(_.averagePrice).max
-//    val range = maxAveragePrice - minAveragePrice
-//
-//    val minAverageDiscount = scores.map(_.averageDiscount).min
-//    val maxAverageDiscount = scores.map(_.averageDiscount).max
-//    val rangeDiscount = maxAverageDiscount - minAverageDiscount
-//
-//    val minAverageProfitMargin = scores.map(_.averageProfitMargin).min
-//    val maxAverageProfitMargin = scores.map(_.averageProfitMargin).max
-//    val rangeProfitMargin = maxAverageProfitMargin - minAverageProfitMargin
-//
-//    // avoid division by zero if all hotels have identical average prices
-//    if range == 0 then
-//      return Some(EconomicScorePercentage(scores.head.hotelName, 0.0))
-//
-//    val finalScores = scores.map { s =>
-//      val averagePriceScore = (s.averagePrice - minAveragePrice) / range
-//      val averageDiscountScore = (s.averageDiscount - minAverageDiscount) / rangeDiscount
-//      val averageProfitMarginScore = (s.averageProfitMargin - minAverageProfitMargin) / rangeProfitMargin
-//
-//      val score = (averagePriceScore + averageDiscountScore + averageProfitMarginScore) / 3
-//
-//      EconomicScorePercentage(s.hotelName, score)
-//    }
-//
-//    // Most economical = smallest price percentage
-//
-//    Some(finalScores.minBy(_.score))
-//  end calculateEconomicScore
+  end calculateEconomicScore
 }
-
-//import scala.math.Fractional.Implicits.infixFractionalOps
-//import scala.math.Integral.Implicits.infixIntegralOps
-//import scala.math.Numeric.Implicits.infixNumericOps
-//
-//object Question2 {
-//  case class EconomicalScore(
-//    hotelName: String,
-//    averagePrice: Double
-//  )
-//
-//  case class EconomicScorePercentage(
-//                                    hoteName: String
-//                                    score: Double
-//                                    )
-//
-//  def calculateEconomicScore(bookings: List[HotelBooking]): Option[EconomicalScore] =
-//    val minPriceOfAllBookings = bookings.map(b => b.bookingPrice / b.rooms / b.noOfDays).min
-//    val maxPriceOfAllBookings = bookings.map(b => b.bookingPrice / b.rooms / b.noOfDays).max
-//
-//    val grouped = bookings.groupBy(_.hotelName)
-//    val scores = grouped.map { case (hotelName, hotelBookings) =>
-//      // Normalise price: price per room per day
-//      val effectivePrices = hotelBookings.map { b =>
-//        b.bookingPrice / b.rooms / b.noOfDays
-//      }
-//      val avgPrice = effectivePrices.sum / effectivePrices.length
-//
-//      val maxminP = 1- (avgPrice / )
-//
-//      val discounts = hotelBookings.map(_.discount)
-//      val minD = discounts.min
-//      val maxD = discounts.max
-//      val maxminD = maxD - minD
-//
-//      val profitMargins = hotelBookings.map(_.profitMargin)
-//      val minPM = profitMargins.min
-//      val maxPM = profitMargins.max
-//      val maxminPM = maxPM - minPM
-//
-//      val score = (-maxminP + maxminD + maxminPM) / 3
-//
-////      val avgDiscount = hotelBookings.map(_.discount).sum / hotelBookings.length
-////      val avgProfitMargin = hotelBookings.map(_.profitMargin).sum / hotelBookings.length
-//
-//      EconomicalScore(
-//        hotelName,
-//        avgPrice
-//      )
-//    }
-//
-//    // Min/Max of averagePrice across all scores
-//    val minAveragePrice = scores.map(_.averagePrice).min
-//    val maxAveragePrice = scores.map(_.averagePrice).max
-//
-//
-//    val finalResult = scores.map { case (hotelName, avgPrice) =>
-//      val averagePercentage = (avgPrice - minAveragePrice) / (maxAveragePrice - minAveragePrice)
-//
-//      EconomicScorePercentage(
-//        hotelName,
-//        averagePercentage
-//      )
-//    }
-//
-//
-//    // Most economical = smallest price variation
-//    Some(EconomicScorePercentage.maxBy(_.averagePercentage))
-//  end calculateEconomicScore
-//}
